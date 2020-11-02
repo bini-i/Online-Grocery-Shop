d48d986ff28594f0bcf9953c77d5d36909386a9d
bini-i <binyam.sh1@gmail.com>
2020-11-02 13:52:54 +0300

    Add animation to overlay section of header
    add transform key frame animation to chnage translateX from 75% to 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
   // main page carousel navigation
-  let carousel_nav_left = document.querySelector('.carousel-nav-left');
-  let carousel_nav_right = document.querySelector('.carousel-nav-right');
-  let carousel = document.querySelector('.carousel');
+//   let carousel_nav_left = document.querySelector('.carousel-nav-left');
+//   let carousel_nav_right = document.querySelector('.carousel-nav-right');
+//   let carousel = document.querySelector('.carousel');
 
-  let carousel_nav_left2 = document.querySelector('.carousel-nav-left2');
-  let carousel_nav_right2 = document.querySelector('.carousel-nav-right2');
-  let carousel2 = document.querySelector('.carousel2');
+//   let carousel_nav_left2 = document.querySelector('.carousel-nav-left2');
+//   let carousel_nav_right2 = document.querySelector('.carousel-nav-right2');
+//   let carousel2 = document.querySelector('.carousel2');
 
-  carousel_nav_left.addEventListener('click', (e) => {
-    let slider = parseInt(carousel.style.left) || 0;
-    if (slider < 0) {
-      carousel.style.left = (slider + 135) + 'px';
-    }
-  });
+//   carousel_nav_left.addEventListener('click', (e) => {
+//     let slider = parseInt(carousel.style.left) || 0;
+//     if (slider < 0) {
+//       carousel.style.left = (slider + 135) + 'px';
+//     }
+//   });
 
-  carousel_nav_right.addEventListener('click', (e) => {
-    let slider = parseInt(carousel.style.left) || 0;
-    if (slider > carousel.childElementCount * -135 / 1.8) {
-      carousel.style.left = (slider - 135) + 'px';
-    } else {
-      carousel.style.left = 0;
-    }
-  });
+//   carousel_nav_right.addEventListener('click', (e) => {
+//     let slider = parseInt(carousel.style.left) || 0;
+//     if (slider > carousel.childElementCount * -135 / 1.8) {
+//       carousel.style.left = (slider - 135) + 'px';
+//     } else {
+//       carousel.style.left = 0;
+//     }
+//   });
 
-  carousel_nav_left2.addEventListener('click', (e) => {
-    let slider = parseInt(carousel2.style.left) || 0;
-    if (slider < 0) {
-      carousel2.style.left = (slider + 135) + 'px';
-    }
-  });
+//   carousel_nav_left2.addEventListener('click', (e) => {
+//     let slider = parseInt(carousel2.style.left) || 0;
+//     if (slider < 0) {
+//       carousel2.style.left = (slider + 135) + 'px';
+//     }
+//   });
 
-  carousel_nav_right2.addEventListener('click', (e) => {
-    let slider = parseInt(carousel2.style.left) || 0;
-    if (slider > carousel2.childElementCount * -135 / 1.8) {
-      carousel2.style.left = (slider - 135) + 'px';
-    } else {
-      carousel2.style.left = 0;
-    }
-  });
+//   carousel_nav_right2.addEventListener('click', (e) => {
+//     let slider = parseInt(carousel2.style.left) || 0;
+//     if (slider > carousel2.childElementCount * -135 / 1.8) {
+//       carousel2.style.left = (slider - 135) + 'px';
+//     } else {
+//       carousel2.style.left = 0;
+//     }
+//   });
 
 //   //main page see more functionality
   let see_more = document.querySelector('.see-more p');
